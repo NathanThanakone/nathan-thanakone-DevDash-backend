7d3b5ae6247fddcc47110df0a5760a4b1d96daad
Nathan Thanakone <nathan.thanakone@gmail.com>
2023-05-16 16:25:52 -0400

    removed unused routes and data files

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
-const goalsRoute = require('./routes/goals');
 const dashboardRoute = require('./routes/dashboard');
-const tasksRoute = require('./routes/tasks');
 
 app.use(cors());
 app.use(express.json());
 
-// route handlers for dashboard and goals routes
+// route handlers for dashboard
 app.use('/dashboard', dashboardRoute);
-app.use('/goals', goalsRoute);
-app.use('/tasks', tasksRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
