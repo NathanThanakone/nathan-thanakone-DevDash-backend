cc1934a61ae85bed3317b83eeea3187cdbe8b7f5
Nathan Thanakone <nathan.thanakone@gmail.com>
2023-05-16 01:14:29 -0400

    removed unused route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,14 +7,6 @@ const API_KEY = process.env.API_KEY;
 const URL = 'https://wakatime.com/api/v1/'
 const codingActivity_URL = 'https://wakatime.com/share/@384bee01-d4f3-4943-8eeb-e225e0a39109/549dc94e-2ca7-4cae-8476-4e59d22a7f1c.json'
 
-router.get('/projects', (req, res) => {
-    axios
-        .get(`${URL}users/current/projects${API_KEY}`)
-        .then(response => {
-            res.json(response.data);
-        })
-})
-
 // parent data array inside response holds all data = response.data.data
 router.get('/codingactivity', (req, res) => {
     axios
